5cbc6b9710e8eaddc6c45749303bf7b0650bd63c
Suzan Alamassi <suzanamassy@gmail.com>
2025-04-28 12:43:40 +0300

    Initial Commit

---
+++ TenTimeMovieOfflineApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TenTimeMovieOfflineApp
+//
+//  Created by Suzan Amassy on 28/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TenTimeMovieOfflineApp/TenTimeMovieOfflineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenTimeMovieOfflineAppApp.swift
+//  TenTimeMovieOfflineApp
+//
+//  Created by Suzan Amassy on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TenTimeMovieOfflineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
